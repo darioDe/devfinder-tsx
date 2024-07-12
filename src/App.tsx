8ea1abfa60d189cb2b0aad9784157ff56f73845d
Rdduarte <rdduarte1811@gmail.com>
2024-07-12 06:45:44 -0300

    add usetheme in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,46 @@
-import { ThemeProvider } from './context/ThemeContext'
+import { ThemeProvider, useTheme } from './context/ThemeContext';
 import Header from './components/Header'
 import './index.css'
 
-function App() {
+const App: React.FC = () => {
+  const { theme, toggleTheme } = useTheme();
+  
   return (
     <ThemeProvider >
-    <div className='h-screen w-screen bg-slate-900'>
-      <div className=''>
-       <Header />
-        <nav>
-          <div>
-            <form action="">
-              <input type='search' />
-              <button type='submit'></button>
-            </form>
-          </div>
-        </nav>
-        <main>
-          <img src="" alt="" />
-          <h2>Name</h2>
-          <h3>User Name</h3>
-          <legend>Bio</legend>
-          <section>
-            <ul>
-              <li>Repos</li>
-              <p>0</p>
-              <li>Followers</li>
-              <p>0</p>
-              <li>Following</li>
-              <p>0</p>
-            </ul>
-          </section>
-          <p></p>
-          <p></p>
-        </main>
-        <footer>
-        </footer>
-      </div>  
-    </div>
-    </ ThemeProvider >
+      <div className= {theme == 'dark-theme' ? 'h-screen w-screen bg-slate-50 py-20': 'h-screen w-screen bg-slate-900 py-20'}>
+        <div className=''>
+        <Header />
+          <nav>
+            <div>
+              <form action="">
+                <input type='search' />
+                <button type='submit'></button>
+              </form>
+            </div>
+          </nav>
+          <main>
+            <img src="" alt="" />
+            <h2>Name</h2>
+            <h3>User Name</h3>
+            <legend>Bio</legend>
+            <section>
+              <ul>
+                <li>Repos</li>
+                <p>0</p>
+                <li>Followers</li>
+                <p>0</p>
+                <li>Following</li>
+                <p>0</p>
+              </ul>
+            </section>
+            <p></p>
+            <p></p>
+          </main>
+          <footer>
+          </footer>
+        </div>  
+      </div>
+    </ThemeProvider >
   )
 }
 
